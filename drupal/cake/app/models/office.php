a8a8f595b80c1c0f8f4553af6f95195c5c1e9a82
Quinn Supplee <qsupplee@gmail.com>
2009-11-11 11:23:04 -0800

    removed cake from wrong location

--- drupal/cake/app/models/office.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Office extends AppModel {
-
-	var $name = 'Office';
-	var $useTable = 'offices';
-	var $primaryKey = 'Office_MLS_ID';
-}
-
--- drupal/cake/app/models/property.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Property extends AppModel {
-
-	var $name = 'Property';
-	var $useTable = 'all_fields';
-	var $primaryKey = 'ML_Number_Display';	
-}
